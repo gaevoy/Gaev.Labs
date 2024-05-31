11dc00cf16bd62650c0897af131f78776e9bab56
Volodymyr Haievyi <vova.gaevoy@gmail.com>
2024-05-31 11:13:37 +0200

    Coin change solver

---
+++ Gaev.Labs.Tests/CoinChangeSolverTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+
+namespace Gaev.Labs.Tests;
+
+// ReSharper disable NotAccessedPositionalProperty.Local
+public class CoinChangeSolverTests
+{
+    [TestCase("1, 2, 5", 11, 3)]
+    [TestCase("25, 10, 5", 30, 2)]
+    [TestCase("9, 6, 5, 1", 11, 2)]
+    public void Solve(string coinsAsString, int amount, int expectedNumberOfCoins)
+    {
+        // Given
+        var coins = coinsAsString.Split(',').Select(int.Parse).ToArray();
+
+        // When
+        var result = SolveCoinChange(amount, coins);
+
+        // Then
+        Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true }));
+        Assert.That(result?.NumberOfCoins, Is.EqualTo(expectedNumberOfCoins));
+    }
+
+    record Node(int Coin, Node? Parent)
+    {
+        public readonly int TotalAmount = Coin + (Parent?.TotalAmount ?? 0);
+        public readonly int NumberOfCoins = (Parent?.NumberOfCoins ?? -1) + 1;
+        public bool CanFit(int amount) => TotalAmount < amount;
+        public bool IsEqualTo(int amount) => TotalAmount == amount;
+    }
+
+    private static Node? SolveCoinChange(int amount, int[] coins)
+    {
+        var queue = new Queue<Node>();
+        queue.Enqueue(new Node(0, null));
+        while (queue.Any())
+        {
+            var node = queue.Dequeue();
+            foreach (var coin in coins)
+            {
+                var child = new Node(coin, node);
+                if (child.IsEqualTo(amount))
+                    return child;
+
+                if (child.CanFit(amount))
+                    queue.Enqueue(child);
+            }
+        }
+
+        return null;
+    }
+}
--- Gaev.Labs.Tests/KnapsackSolverTests.cs
+++ Gaev.Labs.Tests/KnapsackSolverTests.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.Json;
 
-// ReSharper disable NotAccessedPositionalProperty.Global
+// ReSharper disable NotAccessedPositionalProperty.Local
 
 namespace Gaev.Labs.Tests;
 
@@ -11,39 +11,44 @@ public class KnapsackSolverTests
     [TestCase("3, 5, 7, 9, 2, 1", "5, 12, 8, 10, 3, 2", 12, 22)]
     [TestCase("1, 2, 4, 6, 3", "1, 4, 5, 6, 7", 8, 13)]
     [TestCase("6, 7", "10, 20", 5, 0)]
-    public void Solve(string weights, string values, int capacity, int expectedValue)
+    public void Solve(string weightsAsString, string valuesAsString, int capacity, int expectedValue)
     {
         // Given
-        var w = weights.Split(',').Select(int.Parse).ToArray();
-        var v = values.Split(',').Select(int.Parse).ToArray();
+        var weights = weightsAsString.Split(',').Select(int.Parse).ToArray();
+        var values = valuesAsString.Split(',').Select(int.Parse).ToArray();
 
         // When
+        var result = SolveKnapsack(weights, values, capacity);
+
+        // Then
+        Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true }));
+        Assert.That(result?.TotalValue, Is.EqualTo(expectedValue));
+    }
+
+    private static Node? SolveKnapsack(int[] weights, int[] values, int capacity)
+    {
         var nodes = new List<Node>();
-        var root = new Node(default, default, default);
+        var root = new Node(0, 0, null);
         nodes.Add(root);
-        for (var i = 0; i < w.Length; i++)
+        for (var i = 0; i < weights.Length; i++)
         {
-            var weight = w[i];
-            var value = v[i];
+            var weight = weights[i];
+            var value = values[i];
             foreach (var node in nodes.ToList())
             {
                 var child = new Node(weight, value, node);
-                if (child.IsFit(capacity))
+                if (child.CanFit(capacity))
                     nodes.Add(child);
             }
         }
 
-        var resultingNode = nodes.MaxBy(e => e.TotalValue);
-        Console.WriteLine(JsonSerializer.Serialize(resultingNode, new JsonSerializerOptions { WriteIndented = true }));
-
-        // Then
-        Assert.That(resultingNode?.TotalValue, Is.EqualTo(expectedValue));
+        return nodes.MaxBy(e => e.TotalValue);
     }
-}
 
-record Node(int Weight, int Value, Node? Parent)
-{
-    public readonly int TotalWeight = Weight + (Parent?.TotalWeight ?? 0);
-    public readonly int TotalValue = Value + (Parent?.TotalValue ?? 0);
-    public bool IsFit(int capacity) => TotalWeight <= capacity;
+    record Node(int Weight, int Value, Node? Parent)
+    {
+        public readonly int TotalWeight = Weight + (Parent?.TotalWeight ?? 0);
+        public readonly int TotalValue = Value + (Parent?.TotalValue ?? 0);
+        public bool CanFit(int capacity) => TotalWeight <= capacity;
+    }
 }
